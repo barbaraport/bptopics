fb41f739222d2c0082343bfbddc4ed53bfe07b49
Bárbara Port <62317075+barbaraport@users.noreply.github.com>
2022-08-22 09:34:25 -0300

    first commit

---
+++ src/main/java/br/gov/sp/fatec/bptopics/BptopicsApplication.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.bptopics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BptopicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BptopicsApplication.class, args);
+	}
+
+}
