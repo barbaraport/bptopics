ea3dc4d4e0cea9ecfb70e9d0ec8ba4fe8ee5bd02
Bárbara Port <barbara.port@outlook.com>
2022-09-06 13:33:28 +0000

    adding queries

--- src/main/java/br/gov/sp/fatec/bptopics/repository/UsuarioRepository.java
+++ src/main/java/br/gov/sp/fatec/bptopics/repository/UsuarioRepository.java
@@ -1,9 +1,23 @@
 package br.gov.sp.fatec.bptopics.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import br.gov.sp.fatec.bptopics.entity.Usuario;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
     
+    public Usuario findByNome(String nome);
+
+    @Query("select us from Usuario us where us.nome = ?1")
+    public Usuario buscarPorNome(String nome);
+
+    public List<Usuario> findByNomeContains(String nome);
+
+    public List<Usuario> findByAutorizacoesNome(String autorizacao);
+
+    @Query("select us from Usuario us join us.autorizacoes au where au.nome = ?1")
+    public List<Usuario> buscarPorAutorizacao(String autorizacao);
 }
